0704d34ace40508aab20965cc4efce7dfeaa1119
TNgoee <vananh03012004@gmail.com>
2026-07-11 11:21:20 +0700

    Cập nhật mới

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,28 @@
-import express, { type Express } from "express";
+import express, { type Express, type Request, type Response, type NextFunction } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import { type Options, pinoHttp } from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  }),
-);
+const pinoHttpOptions: Options = {
+  logger,
+  serializers: {
+    req: (req: Request) => ({
+      id: req.id,
+      method: req.method,
+      url: req.url?.split("?")[0],
+    }),
+    res: (res: Response) => ({
+      statusCode: res.statusCode,
+    }),
+  },
+};
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  pinoHttp(pinoHttpOptions)(req, res, next);
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
