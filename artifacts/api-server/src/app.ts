793e7025fd97aef8e505c75ff609c8644d271694
TNgoee <vananh03012004@gmail.com>
2026-07-11 11:42:53 +0700

    Make api-server build resilient for Vercel

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,27 +1,16 @@
 import express, { type Express, type Request, type Response, type NextFunction } from "express";
 import cors from "cors";
-import pinoHttp, { type Options } from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-const pinoHttpOptions: Options = {
-  logger,
-  serializers: {
-    req: (req: Request) => ({
-      id: req.id,
-      method: req.method,
-      url: req.url?.split("?")[0],
-    }),
-    res: (res: Response) => ({
-      statusCode: res.statusCode,
-    }),
-  },
-};
-
 app.use((req: Request, res: Response, next: NextFunction) => {
-  pinoHttp(pinoHttpOptions)(req, res, next);
+  logger.info({ method: req.method, url: req.url?.split("?")[0] }, "request received");
+  res.on("finish", () => {
+    logger.info({ statusCode: res.statusCode }, "request completed");
+  });
+  next();
 });
 app.use(cors());
 app.use(express.json());
